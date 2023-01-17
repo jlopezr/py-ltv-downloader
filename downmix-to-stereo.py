e00edbaf35f0fb46e17302f118fcbf26cb286e2a
Juan López Rubio <j.lopez.r@gmail.com>
2023-01-18 00:20:11 +0100

    Update downmix-to-stereo.py

--- downmix-to-stereo.py
+++ downmix-to-stereo.py
@@ -1,11 +1,14 @@
 import subprocess;
 import sys;
+import os.path;
 
 if len(sys.argv)!=3:
     print("downmix-to-stereo requires 1 parameter: filename of input media")
     sys.exit(-1)
 
 input = sys.argv[2]
-output = "stereo-"+input
+dir, file = os.path.split(input)
+file = "stereo-"+file
+output = os.path.join(dir, file)
 
 subprocess.run(["ffmpeg","-i",input,"-c:v","copy","-ac","2",output])
