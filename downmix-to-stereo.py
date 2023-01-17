c4b1ff01c950dfc2de32127eac389cf7e3c844cf
Juan López Rubio <j.lopez.r@gmail.com>
2023-01-18 00:11:49 +0100

    Create downmix-to-stereo.py

---
+++ downmix-to-stereo.py
@@ -0,0 +1,11 @@
+import subprocess;
+import sys;
+
+if len(sys.argv)!=3:
+    print("downmix-to-stereo requires 1 parameter: filename of input media")
+    sys.exit(-1)
+
+input = sys.argv[2]
+output = "stereo-"+input
+
+subprocess.run(["ffmpeg","-i",input,"-c:v","copy","-ac","2",output])⏎
