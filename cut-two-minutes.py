0c0501d14f10c4e507ce6e2b60c92f08f82073ad
Juan López Rubio <j.lopez.r@gmail.com>
2023-01-29 19:45:27 +0100

    Create cut-two-minutes.py

---
+++ cut-two-minutes.py
@@ -0,0 +1,14 @@
+import subprocess;
+import sys;
+import os.path;
+
+if len(sys.argv)!=3:
+    print("cut-two-minutes requires 1 parameter: filename of input media")
+    sys.exit(-1)
+
+input = sys.argv[2]
+dir, file = os.path.split(input)
+file = "cut-"+file
+output = os.path.join(dir, file)
+
+subprocess.run(["ffmpeg","-i",input,"-ss","0","-t","120","-c","copy",output])
